03430cc81cb0ab8f1ea15ebf22da40121411a0c9
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-09-18 10:57:03 +0600

    Shuvankor update prifile

--- src/Pages/ShuvankorVai.jsx
+++ src/Pages/ShuvankorVai.jsx
@@ -7,6 +7,11 @@ const ShuvankorVai: React.FC = () => {
     return (
         <div className="text-center">
             Shuvankor Vai, please write something or do some coding.
+            <ul>
+                <li>Shuvankor Hazra</li>
+                <li>shuvo.s.ovuhs@gmail.com</li>
+                <li>01741-836653</li>
+            </ul>
         </div>
     );
 };
