934c232559c49f1cc9b163eb76abe25db8bf0f6a
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-09-18 12:24:20 +0600

    Shuvankor updated page

--- src/Pages/ShuvankorVai.jsx
+++ src/Pages/ShuvankorVai.jsx
@@ -7,6 +7,14 @@ const ShuvankorVai: React.FC = () => {
     return (
         <div className="text-center">
             Shuvankor Vai, please write something or do some coding.
+            <div className="bg-teal-100 py-10 mt-10 w-1/3 mx-auto rounded">
+                <div className="">
+                    <h2 className="text-xl">Shuvankor Hazra!</h2>
+                    <p className='text-lg'>shuvo.s.ovuhs@gmail.com</p>
+                    <p className='text-lg'>Khulna, Bangladesh</p>
+
+                </div>
+            </div>
         </div>
     );
 };
